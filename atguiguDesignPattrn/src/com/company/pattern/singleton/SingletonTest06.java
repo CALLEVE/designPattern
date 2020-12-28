4f2ae190675932af73a633e49bc7bf5aeb57bb0b
wangjinpeng <wangjinpeng@nbeport.com>
2020-12-28 17:17:09 +0800

    s

--- atguiguDesignPattrn/src/com/company/pattern/singleton/SingletonTest06.java
+++ atguiguDesignPattrn/src/com/company/pattern/singleton/SingletonTest06.java
@@ -1,5 +1,8 @@
 package com.company.pattern.singleton;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 /**
  * @program: atguiguDesignPattrn
  * @author: wangjinpeng
@@ -8,7 +11,18 @@ package com.company.pattern.singleton;
  **/
 public class SingletonTest06 {
 
-    private SingletonTest06(){}
+    private static boolean qinjiang = false;
+
+    private SingletonTest06(){
+        // 这一步加锁，感觉性能会不好
+        synchronized (SingletonTest06.class){
+            if (qinjiang==false){
+                qinjiang = true;
+            }else{
+                throw new RuntimeException("不要使用反射破坏单例");
+            }
+        }
+    }
 
     private static volatile SingletonTest06 INSTANCE;
 
@@ -23,9 +37,22 @@ public class SingletonTest06 {
         return INSTANCE;
     }
 
+    public static void main(String[] args) throws Exception {
+        SingletonTest06 instance1 = SingletonTest06.getInstance();
 
+        SingletonTest06 instance2 = SingletonTest06.getInstance();
 
+//        Constructor<SingletonTest06> declaredConstructor = SingletonTest06.class.getDeclaredConstructor(null);
+//        declaredConstructor.setAccessible(true);
+//        SingletonTest06 instance1 = declaredConstructor.newInstance();
+//        SingletonTest06 instance2 = declaredConstructor.newInstance();
 
-
-
+//        if (instance1==instance2){
+//            System.out.println("yes");
+//        }else{
+//            System.out.println("no");
+//        }
+//        System.out.println(instance1);
+//        System.out.println(instance2);
+    }
 }
